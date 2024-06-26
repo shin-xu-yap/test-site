82f3aec30d12379340ed416de1835c7256cc5410
shinxu <shinxu.yap@gmail.com>
2024-06-26 22:17:51 +0800

    modified hash router

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import HomePage from "./pages/HomePage"
 
 function App() {
     return (
-        <Router>
+        <Router basename="/">
             <Routes>
                 <Route path="/home" element={<HomePage/>}/>
             </Routes>
